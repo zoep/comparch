05c1f9a09081928c7fc6cc258b6e89f68a66284e
Zoe P <zoe.paraskevopoulou@gmail.com>
2012-04-13 22:32:59 +0300

    changes..

--- equake/runequake.sh
+++ equake/runequake.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x equake1.simics exit &> l1results/equake-16384-32-2.txt
-
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x equake.simics exit &> l1results/equake-16384-128-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=256 -e '$LINESZ'=64 -e '$ASSOC'=2 -x art1.simics exit &> l1results/equake-16384-64-2.txt
 
 for linesize in 32 64 128
 do
