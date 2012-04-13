e32f99baaa8a0f711c31fd4544e2dce57c39ae57
Zoe P <zoe.paraskevopoulou@gmail.com>
2012-04-13 22:12:27 +0300

    sripts fixed

--- art/runart.sh
+++ art/runart.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x art1.simics exit &> art-16384-32-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x art1.simics exit &> l1results/art-16384-32-2.txt
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x art.simics exit &> art-16384-128-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x art.simics exit &> l1results/art-16384-128-2.txt
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=4 -x art.simics exit &> art-16384-32-4.txt
+for linesize in 32 64 128
+do
+	linesno=$((16384/$linesize))
+	
+	/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=4 -x art.simics exit &> l1results/art-16384-${linesize}-4.txt
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=4 -x art.simics exit &> art-16384-128-4.txt
+done
+
 
 for cashesize in 32768 65536
 do
-	for linesize in 32 128
+	for linesize in 32 64 128
 	do
 		linesno=$(($cashesize/$linesize))
 		
 		for assoc in 4 8
 	  	do
 	
-			/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=$assoc -x art.simics exit &> art${cashesize}-${linesize}-${assoc}.txt
+			/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=$assoc -x art.simics exit &> l1results/art-${cashesize}-${linesize}-${assoc}.txt
 
 	
 		done
--- equake/runequake.sh
+++ equake/runequake.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x equake1.simics exit &> equake-16384-32-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x equake1.simics exit &> l1results/equake-16384-32-2.txt
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x equake.simics exit &> equake-16384-128-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x equake.simics exit &> l1results/equake-16384-128-2.txt
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=4 -x equake.simics exit &> equake-16384-32-4.txt
+for linesize in 32 64 128
+do
+	linesno=$((16384/$linesize))
+	
+	/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=4 -x equake.simics exit &> l1results/equake-16384-${linesize}-4.txt
 
-/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=4 -x equake.simics exit &> equake-16384-128-4.txt
+done
+
 
 for cashesize in 32768 65536
 do
-	for linesize in 32 128
+	for linesize in 32 64 128
 	do
 		linesno=$(($cashesize/$linesize))
 		
 		for assoc in 4 8
 	  	do
 	
-			/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=$assoc -x equake.simics exit &> equake${cashesize}-${linesize}-${assoc}.txt
+			/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=$assoc -x equake.simics exit &> l1results/equake-${cashesize}-${linesize}-${assoc}.txt
 
 	
 		done
