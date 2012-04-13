84d95c5cb2ff26d7a9f7caf5a271e2192e2730a2
Zoe P <zoe.paraskevopoulou@gmail.com>
2012-04-13 15:20:11 +0300

    changesII..

--- art/runart.sh
+++ art/runart.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-./simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x
-art/art1.simics exit &> art-16384-32-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=2 -x art1.simics exit &> art-16384-32-2.txt
 
-./simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x
-art/art.simics exit &> art-16384-128-2.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=2 -x art.simics exit &> art-16384-128-2.txt
 
-./simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=4 -x
-art/art.simics exit &> art-16384-32-4.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=512 -e '$LINESZ'=32 -e '$ASSOC'=4 -x art.simics exit &> art-16384-32-4.txt
 
-./simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=4 -x
-art/art.simics exit &> art-16384-128-4.txt
+/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=128 -e '$LINESZ'=128 -e '$ASSOC'=4 -x art.simics exit &> art-16384-128-4.txt
 
 for cashesize in 32768 65536
 do
@@ -21,8 +17,7 @@ do
 		for assoc in 4 8
 	  	do
 	
-			./simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=$assoc -x
-			art/art.simics exit &> art${cashesize}-${linesize}-${assoc}.txt
+			/home/zoo/simics-workspace/simics -stall -e '$LINESNO'=$linesno -e '$LINESZ'=$linesize -e '$ASSOC'=$assoc -x art.simics exit &> art${cashesize}-${linesize}-${assoc}.txt
 
 	
 		done
