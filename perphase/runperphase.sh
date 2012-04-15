29c266aeabe2c02a32fb31c5abf6009a5cf1c68e
Zoe P <zoe.paraskevopoulou@gmail.com>
2012-04-15 21:17:00 +0300

    exit removed

--- perphase/runperphase.sh
+++ perphase/runperphase.sh
@@ -3,7 +3,7 @@
 for bench in art gzip equake mfc
 do
 		
-	/home/zoo/simics-workspace/simics -stall -e '$BENCH'=$bench -x perphase.simics exit
+	/home/zoo/simics-workspace/simics -stall -e '$BENCH'=$bench -x perphase.simics
 
 done
 
